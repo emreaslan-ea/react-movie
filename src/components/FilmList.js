29cd42e3d6223f8191fa499694a56f6c573cf8bf
Emre Aslan <emreaslan7@icloud.com>
2022-09-04 12:13:11 +0300

    update

--- src/components/FilmList.js
+++ src/components/FilmList.js
@@ -1,9 +1,36 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
 
 export default class FilmList extends React.Component {
+
   render() {
     return (
-      <div>FilmList</div>
+      <div className="bg-white">
+        <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+
+          <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+
+            {this.props.movies.map((movie) => (
+
+              <div href="#" className="group drop-shadow-md" key={movie.id}>
+                <div className="px-0.5 w-full overflow-hidden">
+                  <img src={movie.imageURL} alt="Movie poster" className="h-full w-full object-cover object-center group-hover:opacity-75 rounded-t-lg" />
+                </div>
+                <div className='p-3 pt-1 -mt-1 border-solid border-slate-200 border-2 rounded-b-lg'>
+                  <h3 className="mt-2 text-lg text-gray-900">{movie.name}</h3>
+                  <p className="mt-1 text-xs text-gray-600">{movie.overview}</p>
+                  <div className='flex justify-between'>
+                    <button onClick={() => this.props.deleteMovieProp(movie)} className='p-0 px-3 mt-3 text-red-500 border-red-300 border-2 rounded-lg hover:bg-red-500 hover:text-white'>Delete</button>
+                    <span className='p-1 px-3 mt-3 bg-sky-500 text-lg text-white rounded-lg'>{movie.rating}</span>
+                  </div>
+                </div>
+              </div>
+
+            ))}
+
+          </div>
+        </div>
+      </div>
     )
   }
 }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,7 +4,7 @@ export default class SearchBar extends React.Component {
   render() {
     return (
         <div>
-            <label className="relative block">
+            <label className="relative block mt-5 mx-6">
                 <span className="absolute inset-y-0 left-0 flex items-center pl-2">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 text-slate-400 ">
                         <path fillRule="evenodd" d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z" clipRule="evenodd" />
