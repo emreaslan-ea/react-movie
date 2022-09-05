cd9083e85ea6baa38ca7d449741dc552db5713a1
Emre Aslan <emreaslan7@icloud.com>
2022-09-06 00:00:29 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import FilmList from './components/FilmList';
 import SearchBar from './components/SearchBar';
+import TrendMovies from './components/TrendMovies';
 import './index.css';
 
 
 export default class App extends React.Component {
+  constructor(){
+    super();
+
+    this.switchTrendScale = this.switchTrendScale.bind(this);
+  }
   state ={
     movies : [],
 
@@ -30,9 +36,45 @@ export default class App extends React.Component {
     
   }
   
+  async switchTrendScale (trendperiod) {
+    if(trendperiod === true){
+      const response = await fetch(`https://api.themoviedb.org/3/trending/all/day?api_key=${process.env.REACT_APP_API_KEY}`);
+      const responseData = await response.json();
+      
+      
+      // if(this.state.movies === responseData.results){
+      //   console.log("days eşit");
+      // }else{
+      //   console.log("days eşit değil");
+      // }
+
+      this.setState({
+        movies : responseData.results
+      })
+
+      console.log("day");
+      console.log(responseData.results);
+
+    }else{
+      const response = await fetch(`https://api.themoviedb.org/3/trending/all/week?api_key=${process.env.REACT_APP_API_KEY}`);
+      const responseData = await response.json();
+      
+      this.setState({
+        movies : responseData.results
+      })
+      console.log("week");
+      console.log(responseData.results);
+      // if(this.state.movies === responseData.results){
+      //   console.log("weeks eşit");
+      // }else{
+      //   console.log("weeks eşit değil");
+      // }
+
+    }
+  }
+
   async componentDidMount(){
     
-
     const response = await fetch(`https://api.themoviedb.org/3/trending/all/day?api_key=${process.env.REACT_APP_API_KEY}`);
     const responseData = await response.json();
     
@@ -50,9 +92,12 @@ export default class App extends React.Component {
       else{return movie.name.toLowerCase().indexOf(this.state.inputSearch.toLowerCase()) !== -1;}
     })
 
+
+    
     return (
       <div className="lg:w-4/5 w-full mx-auto ">
         <SearchBar inputData = {this.searchMovie}/>
+        <TrendMovies trendMovies = {filteredMovies} trendScale ={this.switchTrendScale}/>
         <FilmList movies={filteredMovies} deleteMovieProp={this.DeletedMovie}/>
       </div>
     );
